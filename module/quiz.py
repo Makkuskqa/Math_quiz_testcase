f403c4c9f70d057808f5c1541682a6641f1e7a38
python-rocket <131248689+python-rocket@users.noreply.github.com>
2023-04-19 12:52:42 +0000

    some minor changes

--- module/quiz.py
+++ module/quiz.py
@@ -5,16 +5,5 @@ def get_questions_and_answers(topic):
   """Fill out the function. """
 
 
-def answer_question(name: str, topic: str) -> bool:
-    question_and_answer = get_questions_and_answers(topic)
-    question = question_and_answer[0]
-    correct_answer = question_and_answer[1]
-    print(f"--- {name}, What is the result of following math equation: ---")
-    print(question + "\n")
-    user_answer = input("Enter a whole number from 0-100: ")
-    if user_answer == correct_answer:
-        print("\n--> The answer is correct.")
-        return True
-    else:
-        print(f"\n--> The answer is incorrect. The correct answer is: {correct_answer}")
-        return False
+def answer_question(name: str, topic: str):
+  """Fill out the function. """
